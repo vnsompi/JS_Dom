799f7770a73ecacc4f093898ca2f161759a401d6
vainqueur <vnsompi@gmail.com>
2025-07-25 12:30:30 +0100

    partie2 Js Dom

---
+++ index.js
@@ -0,0 +1,231 @@
+// comment pointer les element 
+// les differents selecteurs 
+
+// .comment selctionner les element 
+// document.querySelector('h4').style.backgroundColor = 'red';
+
+
+// const baliseH4 = document.querySelector('h4');
+// console.log(baliseH4);
+
+// // baliseH4.style.backgroundColor = 'yellow';
+
+// // les elements sur les click
+// // autrement appeler les click event
+// const btn1 = document.querySelector('#btn-1');
+
+// // UNE AUTRE MANIERE
+// const btn2  = document.getElementById('btn2');
+
+const reponse = document.querySelector('p');
+// // console.log('la reponse est ',reponse);
+
+const questionQ = document.querySelector('.click_event');
+// console.log(questionQ); 
+
+
+// ici js sait qu'il doit trouver la variable
+// comement selectionner les elements par ID
+
+
+
+// btn1.addEventListener("click", () =>{
+//    reponse.classList.add("show_response")
+//    reponse.style.background = "green";
+// })
+
+
+// // differencier le bonne reponse a la mauvaise 
+
+// btn1.addEventListener("click", () =>{
+//    response.style.visibility  = "visible"
+// //    reponse.style.background = "red";
+// })
+// questionQ.addEventListener('click',() => {
+//    console.log('clicked!');
+// })
+// un message en cliquant sur la question
+// questionQ.addEventListener('click',() => {
+//    console.log('clicked!');
+  
+// });
+
+//  btn1.addEventListener("click", ()=>{
+//    console.log('i have been clicked');
+//  });
+
+// btn2.addEventListener("click", ()=>{
+//    console.log('i have been clicked');
+// });
+
+
+
+// maintenant reveler la reponse 
+
+
+
+// -------------------------------------------------------------
+// 2 Parlons de Mouse move
+
+// MOUSE EVENT 
+// cree un petit cercle qui va nous suivre partout
+
+// avant de l'utiliser il faut la pointer 
+const  mousemove  =  document.querySelector(".mousemove");
+// console.log(mousemove);
+
+// pour faire  l'evenement  avec la souris
+window.addEventListener('mousemove',(e) =>{
+   mousemove.style.left = e.pageX +'px';
+   mousemove.style.top = e.pageY +'px';
+
+   /* une proprieté qui s'appele x et y */
+   // mousemove.style.left = e.y +'px';
+   // mousemove.style.top = e.y +'px';
+   // ici le cercle ne vas pas suivre 
+})
+
+// les autre evenement aavec la souris
+
+
+window.addEventListener("mousedown", () =>{
+   mousemove.style.transform = "scale(2) translate(-25%, -25%)";
+
+});
+
+window.addEventListener("mousedUp", () =>{
+   mousemove.style.transform = "scale(2) translate(-50%, -50%)";
+   mousemove.style.border = "2px solid teal";
+});
+
+questionQ.addEventListener('mouseenter',()=>{
+   questionQ.style.background = "rgba(0,0,0,0.6)";
+} );
+// la couleur de font en pink
+questionQ.addEventListener("mouseout", ()=>{
+   questionQ.style.background = "pink";
+});
+
+// en cas de survole de la souris 
+reponse.addEventListener("mouseover",()=>{
+   reponse.style.transform = "rotate(2deg)";
+});
+
+// """"""""""""""""""""""""""""""""""""""""""""""""""""""
+
+
+// le key press event
+const keyppress = document.querySelector(".keypress");
+const key = document.getElementById("key");
+// un declanchement a partir de la touche 
+document.addEventListener("keypress", (e)=>{
+   //   savoir suee quelle ouche que nous avons touché
+   // console.log(e.key);
+
+   // pour ecrir du text dans la balise il faut 
+// identifier la balise 
+   key.textContent   = e.key;
+   // conditionner les couleurs en fonction de la touche
+   // si la touche est a ou b alors la couleur de fond sera rouge
+   if(e.key === "a"){
+      keyppress.style.background = "red";
+   } else if (e.key === "b"){
+      keyppress.style.background = "red";
+   }else{
+      keyppress.style.background = "blue";
+   }
+});
+
+// 
+
+// comment ajouter les sons
+// const ring = (key) => {
+//    const audio = new Audio();
+//    audio.src =  key + ".mp3";
+//    audio.play();
+// }
+
+// ring("click");
+// pour jouer le son en fonction de la touche
+// -----------------------------------------------------------------
+// le scroll event
+
+const nav = document.querySelector("nav");
+console.log(nav);
+
+window.addEventListener("scroll", () => {
+   console.log(window.scrollY);
+   // window.scrollY permet de savoir la position du scroll
+// si la position du scroll est superieur a 100px alors on change la couleur de
+
+if (window.scrollY > 120) {
+      nav.style.top = "0";
+   } else {
+      nav.style.top = "-50px";
+   }
+
+});
+
+// -----------------------------------------------------------------
+// LES EVENEMENT SUR LES FORMULAIRES
+// et les inputs
+// les form event 
+const inputName = document.querySelector(".box input[type='text']");
+const select = document.querySelector("select");
+const form = document.querySelector("form");
+ let pseudo = "";
+ let language = "";
+
+
+
+inputName.addEventListener("input", (e) => {
+   pseudo = (e.target.value);
+   // e.target.value permet de recuperer la valeur de l'input
+   // on peut aussi utiliser e.target.value pour changer la valeur de l'input
+
+   // on peut stocker les donnes dans une boite
+   // dans une boite evolutive
+   console.log(pseudo);
+});
+select.addEventListener("input",(e) => {
+   language = (e.target.value);
+   
+})
+
+
+form.addEventListener("submit", (e) => {
+   e.preventDefault(); // pour empecher le rechargement de la page
+   // console.log("le formulaire a ete soumis");
+   // console.log("yess!!");
+   console.log(cgv.checked);
+
+   if (cgv.checked) {
+      // ON PEUT ALLEZ CHERCHER UNE BALISE  AVEC
+      document.querySelector('form > div').innerHTML  = `Bonjour ${pseudo}, vous avez choisi le langage ${language}`;
+   }else{
+      alert("Veuillez remplir tous les champs");
+
+
+   }
+});
+
+
+// -----------------------------------------------------------------
+// le load event
+// le load event se declenche lorsque la page est chargée
+
+window.addEventListener("load", () => {
+   console.log("document chargé");
+});
+
+// --------------------------------------------------
+// le forEach event
+
+const boxes = document.querySelectorAll("box");
+
+boxes.forEach((box) => {
+   box.addEventListener("click", (e) => {
+      console.log(e.target);
+   });
+});
+
